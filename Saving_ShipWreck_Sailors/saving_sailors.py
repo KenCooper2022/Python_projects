81027fb5915a36c94c5e8426d03d1b2092b08f3b
KenCooper <68677207+KenCooper2022@users.noreply.github.com>
2024-02-09 04:30:29 -0500

    Create saving_sailors.py

---
+++ Saving_ShipWreck_Sailors/saving_sailors.py
@@ -0,0 +1,5 @@
+#this project incorporates Bayes theorem
+#originally created to prove the existence of God 
+#it's a theorem that is aimed at creating probabilities given the discovery of new data
+#it has boundless applications such as for example:
+#accounting for the frequency of false positive diagnoses within a study of cancer patients

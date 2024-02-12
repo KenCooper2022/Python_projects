8a54856ca72af077d796ce819a59660df5b8c224
KenCooper <68677207+KenCooper2022@users.noreply.github.com>
2024-02-11 22:57:07 -0500

    file addition and imports
    added a map and made imports to newly installed

--- Saving_ShipWreck_Sailors/saving_sailors.py
+++ Saving_ShipWreck_Sailors/saving_sailors.py
@@ -3,3 +3,9 @@
 #it's a theorem that is aimed at creating probabilities given the discovery of new data
 #it has boundless applications such as for example:
 #accounting for the frequency of false positive diagnoses within a study of cancer patients
+import sys
+import random
+import itertools
+import numpy as np
+import cv2 as cv
+MAP_FILE = 'cape_python.png'
